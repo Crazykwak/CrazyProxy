be802597b2a21b89df3e039656205ae7053fe3ab
Crazykwak <90305019+Crazykwak@users.noreply.github.com>
2024-11-08 10:51:49 +0900

    feat : 깜빡하고 커스텀스레드 add안함. 추가

---
+++ src/main/java/org/crazyproxy/handler/CustomeThread.java
@@ -0,0 +1,21 @@
+package org.crazyproxy.handler;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.crazyproxy.config.Config;
+
+import java.nio.ByteBuffer;
+
+@Slf4j
+@Getter
+public class CustomeThread extends Thread {
+
+    private ByteBuffer myAppData = ByteBuffer.allocate(Config.getInstance().getBufferSize());
+    private ByteBuffer myNetData = ByteBuffer.allocate(Config.getInstance().getBufferSize());
+    private ByteBuffer peerAppData = ByteBuffer.allocate(Config.getInstance().getBufferSize());
+    private ByteBuffer peerNetData = ByteBuffer.allocate(Config.getInstance().getBufferSize());
+
+    public CustomeThread(Runnable target) {
+        super(target);
+    }
+}
