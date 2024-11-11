8b314606d85dc3b5013841316b9c73897d0e4789
Crazykwak <90305019+Crazykwak@users.noreply.github.com>
2024-11-11 15:49:22 +0900

    fix : 코드 쪼끔 정리 및 initSocketInfoHashMap() 수정. path 제대로 뜯도록

--- src/main/java/org/crazyproxy/CrazyProxy.java
+++ src/main/java/org/crazyproxy/CrazyProxy.java
@@ -8,10 +8,7 @@ import org.crazyproxy.nio.SelectorThread;
 import org.crazyproxy.config.SocketInfo;
 
 import javax.net.ssl.*;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.net.InetSocketAddress;
 import java.security.*;
 import java.security.cert.CertificateException;
@@ -32,26 +29,24 @@ public class CrazyProxy {
         Config.initInstance(portMap);
 
         // todo. keyFilePath use -D option
-        String keyFilePath = "keystore.p12";
+        String keyFilePath = System.getProperty("org.crazyproxy.keyFilePath", "server.jks");
         File keyFile = new File(keyFilePath);
+        SSLKeyInfo sslKeyInfo = null;
         if (keyFile.exists()) {
             log.info("Key file exists. try to set SSL Key");
-            SSLKeyInfo sslKeyInfo = setSSLKeyInfo(keyFilePath);
+            sslKeyInfo = setSSLKeyInfo(keyFilePath);
             log.info("Key set done.");
-            try {
-                SSLConfig sslConfig = SSLConfig.getInstance();
-                SSLContext sslContext = sslConfig.getContext();
-                TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-                trustManagerFactory.init((KeyStore) null);
-                sslContext.init(sslKeyInfo.getKeyManagerFactory().getKeyManagers(), trustManagerFactory.getTrustManagers(), new SecureRandom());
-                SSLSession session = sslContext.createSSLEngine().getSession();
-                session.invalidate();
 
+            String trustedCert = System.getProperty("org.crazyproxy.trustedCert", "trustedCerts.jsk");
+            File trustedCertFile = new File(trustedCert);
+            TrustManager[] trustManagers = null;
+            SSLConfig sslConfig = SSLConfig.getInstance();
+            SSLContext sslContext = sslConfig.getContext();
+
+            try {
+                trustManagers = createTrustManager(trustedCertFile, "storepass");
+                sslContext.init(sslKeyInfo.getKeyManagerFactory().getKeyManagers(), trustManagers, new SecureRandom());
             } catch (KeyManagementException e) {
-                throw new RuntimeException(e);
-            } catch (NoSuchAlgorithmException e) {
-                throw new RuntimeException(e);
-            } catch (KeyStoreException e) {
                 throw new RuntimeException(e);
             }
         }
@@ -65,9 +60,9 @@ public class CrazyProxy {
         try {
             KeyStore keyStore = KeyStore.getInstance("JKS");
             FileInputStream keyStoreStream = new FileInputStream(keyFilePath);
-            keyStore.load(keyStoreStream, "qwerty".toCharArray());
+            keyStore.load(keyStoreStream, "storepass".toCharArray());
             KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
-            keyManagerFactory.init(keyStore, "qwerty".toCharArray());
+            keyManagerFactory.init(keyStore, "keypass".toCharArray());
             SSLKeyInfo.initInstance(keyManagerFactory);
             return SSLKeyInfo.getInstance();
         } catch (KeyStoreException e) {
@@ -86,6 +81,48 @@ public class CrazyProxy {
         }
     }
 
+    private static TrustManager[] createTrustManager(File trustedCertFile, String keystorePassword) {
+        InputStream trustStoreIS = null;
+        KeyStore trustStore = null;
+        if (trustedCertFile.exists()) {
+            try {
+                trustStore = KeyStore.getInstance("JKS");
+                trustStoreIS = new FileInputStream(trustedCertFile.getAbsolutePath());
+                trustStore.load(trustStoreIS, keystorePassword.toCharArray());
+            } catch (CertificateException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (NoSuchAlgorithmException e) {
+                throw new RuntimeException(e);
+            } catch (KeyStoreException e) {
+                throw new RuntimeException(e);
+            } finally {
+                if (trustStoreIS != null) {
+                    try {
+                        trustStoreIS.close();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
+
+        TrustManagerFactory trustFactory = null;
+        try {
+            trustFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            trustFactory.init((KeyStore) trustStore);
+        } catch (KeyStoreException e) {
+            throw new RuntimeException(e);
+        }
+        return trustFactory.getTrustManagers();
+
+    }
+
     private static Map<String, SocketInfo> initSocketInfoHashMap() {
         final Map<String, SocketInfo> portMap = new HashMap<>();
 
@@ -101,36 +138,41 @@ public class CrazyProxy {
                 String path = "/";
                 boolean isHttps = false;
                 int targetPort = 80;
+
+                // 일단 https, http 여부 부터 체크.
                 if (host.startsWith("https://")) {
                     targetPort = 443;
                     isHttps = true;
                 }
+
+                // 체크 후에 없애준다. 그래야 inet이 먹음
                 host = host.replace("https://", "");
                 host = host.replace("http://", "");
+
+                // 포트 정보 있는지 체크. http:를 없앴기 때문에 동작함.
                 int portIdx = host.lastIndexOf(":");
                 if (portIdx != -1) {
                     String portInfo = host.substring(portIdx + 1);
-                    if (portInfo.contains("/")) {
-                        String[] split = portInfo.split("/", 2);
-                        portInfo = split[0];
-                        path += split[1];
-                    }
                     targetPort = Integer.parseInt(portInfo);
                     host = host.substring(0, portIdx);
                 }
 
+                // path 정보 추출
+                int pathIndex = host.indexOf("/");
+                if (pathIndex != -1) {
+                    path = host.substring(pathIndex);
+                    host = host.substring(0, pathIndex);
+                }
+
+                // Innet객체 생성하여 socketInfo를 만들어주자.
                 InetSocketAddress address = new InetSocketAddress(host, targetPort);
                 portMap.put(port, new SocketInfo(address, path, isHttps));
+
             }
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
-        }
-
-        for (String s : portMap.keySet()) {
-            System.out.println("s = " + s);
-            System.out.println("portMap = " + portMap.get(s));
         }
         return portMap;
     }
